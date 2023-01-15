ef2d424830fdd13692abe1ea392de1154b5c924c
williamviktorsson <williamviktorsson@gmail.com>
2023-01-15 20:58:54 +0000

    Deploying to gh-pages from @ williamviktorsson/maja_one_2023_webdev@89d367bdfbd150407eb7f15e68c7daedbf1e8255 🚀

--- _app/immutable/chunks/2-958a4c9b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-9d8914fa.js";export{t as component};
---
+++ _app/immutable/chunks/2-e88f9b3c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-be519c86.js";export{t as component};
